209f09d05c3329a75e156346e0f5a7e36067f745
Ben Hichem <77390862+benhichem@users.noreply.github.com>
2023-05-08 11:53:38 +0000

    ...

--- src/componenets/save_data.ts
+++ src/componenets/save_data.ts
@@ -1,3 +1,13 @@
-import fs from "fs";
+import ObjectsToCsv from "objects-to-csv";
+import { company_header } from "../types";
+import Logger from "../misc/Loger.js"
+export default class Save_data {
+  public static async save_company(data: company_header[]) {
+    try {
+      const csv = new ObjectsToCsv(data);
+      await csv.toDisk("../output/company.csv");
+    } catch (err) {
 
-export default class save_data {}
+    }
+  }
+}
